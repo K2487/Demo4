8c871925442d12b41083455eadcfc896b834f52d
Olli <Olli@OLLI-PC>
2017-02-09 11:42:12 +0200

    02

---
+++ AmplifierApplication/Amplifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VahvistinApplication
+{
+    class Amplifier
+    {
+        private readonly int minVolume = 1;
+        private readonly int maxVolume = 100;
+        private int volume { get; set; }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= 100 && value >= 1)
+                {
+                    volume = value;
+                }
+
+                else if (value <= minVolume)
+                {
+                    Console.WriteLine("Too low volume - Amplifier volume is set to minimum : 0");
+                    volume = minVolume;
+                }
+                else if (value >= maxVolume)
+                {
+                    Console.WriteLine("Too much volume -  Amplifier volume is set to maximum : 100");
+                    volume = maxVolume;
+                }
+            }
+        }
+    }
+}
---
+++ AmplifierApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VahvistinApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Amplifier amplifier = new Amplifier();
+            amplifier.Volume = 1;
+            int check = 1;
+            while (check != 0)
+            {
+                Console.Write("Give a new volume value (0-100) > ");
+                string line = Console.ReadLine();
+                amplifier.Volume = int.Parse(line);
+                check = int.Parse(line);
+                Console.WriteLine("Amplifier volume is set to : " + amplifier.Volume);
+                Console.WriteLine();
+            }
+        }
+    }
+}
